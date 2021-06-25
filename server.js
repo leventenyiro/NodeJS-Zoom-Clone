1f6d3cbe2955ed4643a091e25d78659d3de57f61
Levente Nyírő <nyiro.levente@gmail.com>
2021-06-25 22:53:04 +0200

    add new stuffs

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
+const io = require('socket.io')(server)
 const { v4: uuidv4 } = require('uuid')
+const { ExpressPeerServer } = require('peer')
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+})
+
+app.use('/peerjs', peerServer)
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
 
 app.get('/', (req, res) => {
     //res.status(200).send("Hello World!")
@@ -17,6 +26,13 @@ app.get('/:room', (req, res) => {
     })
 })
 
-
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        //console.log("Joined room")
+        socket.join(roomId)
+        socket.broadcast.to(roomId).emit('user-connected', userId)
+        //socket.to(roomId).broadcast.emit('user-connected', userId);
+    })
+})
 
 server.listen(3030)
